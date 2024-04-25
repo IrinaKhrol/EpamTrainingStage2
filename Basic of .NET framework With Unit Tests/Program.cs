86efca0a8d65c438f5aeb14bba7855184ac86397
Irina Khrol <Irina@gmail.com>
2024-04-25 17:33:42 +0300

    Add project files.

---
+++ Basic of .NET framework With Unit Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_of_.NET_framework_With_Unit_Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
